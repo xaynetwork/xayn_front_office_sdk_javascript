90d5fbb456b741f944f3cc766c338030bb1b22a8
ci <ci@xayn.com>
2022-12-22 15:58:33 +0000

    Sync from codegen: version [1.0.16]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * Front Office API
- * # Front Office The system identifies a user with only an id. There are two main endpoints: one that allows registering an interaction between a user and a document (a user clicked or liked a document), and one to retrieve a personalized set of documents for the user. The system needs a few user interactions with documents before it can generate a personalized list.  # Auth To authenticate with the API, a token will be provided.
+ * # Front Office The front office is typically used within front-end apps, for example a website or a mobile application. With this SDK, you can handle interactions with documents and request a list of personalized documents.  ## User Each method requires a `user_id`. From our perspective, a `user_id` is simply required to group interactions together. We don't need to know who that user is, so it is preferred to create this `user_id` in a privacy-preserving way. For example, create a hash method which transforms your user into an ID hash.  ## Document A document here is represented by its `id` and optional `properties`. In order to display a document in an interface, it's preferred to use `properties`, which is an arbitrary collection of values. For example, it could contain an image url, a full article url, the publication date, the author or much more. The properties that were added when the document was submitted to us, are just mirrored here.  ## Auth The API requires an authenticationToken when used. Please see further down in this README.md file, or refer to the test included in this SDK's codebase, on how to set this token correctly. \\
  *
  * The version of the OpenAPI document: 1.0.0-rc6
  * 
@@ -27,7 +27,7 @@ import FrontOfficeApi from './com.xayn.frontoffice/FrontOfficeApi';
 
 
 /**
-* # Front Office The system identifies a user with only an id. There are two main endpoints: one that allows registering an interaction between a user and a document (a user clicked or liked a document), and one to retrieve a personalized set of documents for the user. The system needs a few user interactions with documents before it can generate a personalized list.  # Auth To authenticate with the API, a token will be provided..<br>
+* # Front Office The front office is typically used within front-end apps, for example a website or a mobile application. With this SDK, you can handle interactions with documents and request a list of personalized documents.  ## User Each method requires a &#x60;user_id&#x60;. From our perspective, a &#x60;user_id&#x60; is simply required to group interactions together. We don&#39;t need to know who that user is, so it is preferred to create this &#x60;user_id&#x60; in a privacy-preserving way. For example, create a hash method which transforms your user into an ID hash.  ## Document A document here is represented by its &#x60;id&#x60; and optional &#x60;properties&#x60;. In order to display a document in an interface, it&#39;s preferred to use &#x60;properties&#x60;, which is an arbitrary collection of values. For example, it could contain an image url, a full article url, the publication date, the author or much more. The properties that were added when the document was submitted to us, are just mirrored here.  ## Auth The API requires an authenticationToken when used. Please see further down in this README.md file, or refer to the test included in this SDK&#39;s codebase, on how to set this token correctly. \\.<br>
 * The <code>index</code> module provides access to constructors for all the classes which comprise the public API.
 * <p>
 * An AMD (recommended!) or CommonJS application will generally do something equivalent to the following:
@@ -55,7 +55,7 @@ import FrontOfficeApi from './com.xayn.frontoffice/FrontOfficeApi';
 * </pre>
 * </p>
 * @module index
-* @version 1.0.15
+* @version 1.0.16
 */
 export {
     /**
